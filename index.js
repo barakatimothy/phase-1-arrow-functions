02886b0e4a7010f5a40546f9f672b762914b2e7f
barakatimothy <timothybaraka39@gmail.com>
2023-06-13 12:23:08 -0400

    Complete!

---
+++ index.js
@@ -0,0 +1,4 @@
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+const divide = (param1 , param2) => 2000 / 100;
+const square =(param1) => param1 * param1;
+const add = (param1 , param2) => param1 + param2;
